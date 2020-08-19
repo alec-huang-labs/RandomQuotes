db378cd37ae7d74f6c3d0d95429f34ca0adc53b9
Alec Huang <ahsihuang@gmail.com>
2020-08-19 17:28:52 -0400

    Updates

---
+++ precache-manifest.46fda0fd5483896082ad3a793e3f5120.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81260cf29131d33210d9ef2ef86c2684",
+    "url": "/RandomQuotes/index.html"
+  },
+  {
+    "revision": "1e237d5b730f720ab5d8",
+    "url": "/RandomQuotes/static/css/main.af614c11.chunk.css"
+  },
+  {
+    "revision": "0ac27ccecaca5553eea2",
+    "url": "/RandomQuotes/static/js/2.640f7b8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RandomQuotes/static/js/2.640f7b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e237d5b730f720ab5d8",
+    "url": "/RandomQuotes/static/js/main.65a6fcf5.chunk.js"
+  },
+  {
+    "revision": "f8d28dd10aff53a7a090",
+    "url": "/RandomQuotes/static/js/runtime-main.046fa104.js"
+  }
+]);
